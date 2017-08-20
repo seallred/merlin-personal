112403d578ea3e66f329ff1c7f801234e460a6b6
Scott Allred <sallred@us.ibm.com>
2017-08-19 20:31:31 -0500

    Contact table loaded

--- parature/contact/customer_import.sh
+++ parature/contact/customer_import.sh
@@ -3,9 +3,5 @@ gzip -d CAMDF.PARATURE_CONTACT_PRESTG.IXF.gz
 
 db2 connect to etl2sf
 db2 "IMPORT FROM CAMDF.PARATURE_CONTACT_PRESTG.IXF OF IXF COMMITCOUNT 50000 
-INSERT INTO CAMDF.PARATURE_CONTACT(
-   customer_id, email, first_name, last_name, role, status, 
-   phone_number, alternate_phone_number, contact_country,
-   preferred_language, timezone, account_id, 
-   date_created, date_modified, date_last_visited)" >> IMPORT_PARATURE_CONTACT_PRESTG.ERR
-gzip CAMDF.PARATURE_CONTACT_PRESTG.IXF
+INSERT INTO CAMDF.PARATURE_CONTACT(customer_id, email, first_name, last_name, role, status, phone_number, alternate_phone_number, contact_country, preferred_language, timezone, account_id, date_created, date_modified, date_last_visited)" >> IMPORT_PARATURE_CONTACT_PRESTG.ERR
+#gzip CAMDF.PARATURE_CONTACT_PRESTG.IXF
--- parature/contact/mappingContactPreToStage.sql
+++ parature/contact/mappingContactPreToStage.sql
@@ -1,6 +1,6 @@
 -- Map and load parature_contact_prestg to parature_contact
 
-select count(*) from camdf.parature_contact;
+select count(*) from camdf.parature_contact with ur;
 select * from camdf.parature_contact fetch first 100 rows only;
 select count(*) from camdf.parature_contact_prestg;
 select * from camdf.parature_contact_prestg fetch first 100 rows only;
@@ -29,7 +29,13 @@ select customer_id, count(*) from camdf.parature_contact_custom_prestg cc where 
 select customer_id, count(*) from camdf.parature_contact_custom_prestg cc where customer_custom_field_name = 'PREFERRED_LANGUAGE'  and customer_custom_field_value is not null group by customer_id having count(*) > 1;
 select customer_id, count(*) from camdf.parature_contact_custom_prestg cc where customer_custom_field_name = 'TIMEZONE'  and customer_custom_field_value is not null group by customer_id having count(*) > 1;
 
-select * from camdf.parature_contact_custom_prestg where customer_id = 7839171;
+select * from camdf.parature_contact_custom_prestg where CUSTOMER_ID IN (7839171,7839174,7839175);
+
+-- Issue 3. Bad character in this causing issue with insert.  Truncated.
+select * from camdf.parature_contact_custom_prestg where CUSTOMER_CUSTOM_FIELD_VALUE like '900 906 076%' with ur;  -- had to update this one
+
+select * from camdf.parature_contact where CUSTOMER_ID IN (7839171,7839174,7839175);
+
 
 --truncate table camdf.parature_contact immediate;
 
@@ -38,13 +44,15 @@ insert into camdf.parature_contact (
    phone_number, alternate_phone_number, contact_country,
    preferred_language, timezone, account_id, 
    date_created, date_modified, date_last_visited)
-select 
+select distinct 
    c.customer_id, email, first_name, last_name, customer_role, status, 
-   (select distinct substr(customer_custom_field_value,1,50) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'PHONE_NUMBER') as phone_number,
-   (select substr(customer_custom_field_value,1,40) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'ALTERNATE_PHONE_NUMBER') as alternate_phone_number,
-   (select substr(customer_custom_field_value,1,256) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'CONTACT_COUNTRY') as contact_country,
-   (select substr(customer_custom_field_value,1,64) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'PREFERRED_LANGUAGE') as preferred_language,
-   (select substr(customer_custom_field_value,1,64) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'TIMEZONE') as timezone,
+   (select substr(customer_custom_field_value,1,45) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'PHONE_NUMBER' and customer_custom_field_value is not null fetch first row only) as phone_number,
+   (select substr(customer_custom_field_value,1,40) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'ALTERNATE_PHONE_NUMBER' and customer_custom_field_value is not null) as alternate_phone_number,
+   (select substr(customer_custom_field_value,1,256) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'CONTACT_COUNTRY' and customer_custom_field_value is not null) as contact_country,
+   (select substr(customer_custom_field_value,1,64) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'PREFERRED_LANGUAGE' and customer_custom_field_value is not null) as preferred_language,
+   (select substr(customer_custom_field_value,1,64) from camdf.parature_contact_custom_prestg cc where cc.customer_id = c.customer_id and customer_custom_field_name = 'TIMEZONE' and customer_custom_field_value is not null) as timezone,
    account_id, 
    date_created, date_updated, date_visited
-from camdf.parature_contact_prestg c;
+from camdf.parature_contact_prestg c
+where not exists (select 1 from camdf.parature_contact t where c.customer_id = t.customer_id)
+fetch first 250000 rows only;
